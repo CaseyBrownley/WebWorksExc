84188354e106c43f5654bf07e5abafe6f8e279f9
CaseyBrownley <162181390+CaseyBrownley@users.noreply.github.com>
2024-05-29 17:02:05 -0400

    Add files via upload

---
+++ WebWorks/WebWorks.js
@@ -0,0 +1,29 @@
+//What is HTTP?
+//hyper text transfer protocal is how data gets sent and recieved from server
+
+//What is a URL?
+// uniform resource locator, it is a address for an internet source
+
+//What is DNS?
+//Domain Name system, it takes URLs and turns them into IP addresses
+
+//What is a query string?
+//Allows key-value pairs into the URL
+
+//List two HTTP Verbs and their use cases.
+//Get recieves data from server. Post sends data to server
+
+//What is an HTTP request?
+//A request from client to server with following http protocol
+
+//What is an HTTP response?
+//a response from server to client following http protocol
+
+//What is an HTTP header? Give a couple examples of request and response headers you have seen.
+//Headers provide information about requests/responses in the form of elements like Host or last-modified
+
+//What happens when you type a URL in a browser?
+// the browsers turns the URL into an IP address with DNS. Then is makes a request to the IP address. Then, the server sends a reponse. 
+//The broswer makes a DOM from HTML. It finds other resources by making requests and recieving responses from the server.
+
+
